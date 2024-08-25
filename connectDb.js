9f53179072f09044afb4ea71b67f5f4f849ff30a
tharak1 <saitharakreddyv59@gmail.com>
2024-08-25 10:26:50 +0530

    modified:   connectDb.js

--- connectDb.js
+++ connectDb.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 const connectDb = ()=>{
-    mongoose.connect("mongodb://127.0.0.1:27017/vishesh").then(()=>console.log("database connected")).catch((err)=>console.log(err));
+    mongoose.connect("mongodb+srv://saitharakreddyv59:tharak@tharak.k1lxca6.mongodb.net/vishesh").then(()=>console.log("database connected")).catch((err)=>console.log(err));
     // const connect =  mongoose.connect("mongodb://127.0.0.1:27017/vishesh");
     //     console.log("database connection is established ");
 }
